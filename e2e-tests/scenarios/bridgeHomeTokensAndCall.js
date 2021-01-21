0104800a334fc4a0a60b92978a9478435ec7b247
Kirill Fedoseev <k.fedoseev@innopolis.university>
2021-01-21 16:57:41 +0300

    Update e2e-tests for relay-and-call operations

--- e2e-tests/scenarios/bridgeHomeTokensAndCall.js
+++ e2e-tests/scenarios/bridgeHomeTokensAndCall.js
@@ -8,21 +8,27 @@ async function run({ foreign, home }) {
   const value = toWei('10')
 
   console.log('Sending 10 tokens to the Home Mediator with extra data')
-  await token.methods.approve(mediator.options.address, value).send()
-  const receipt1 = await mediator.methods
-    .relayTokensAndCall(token.options.address, tokenReceiver.options.address, value, '0x1122')
+  const receipt1 = await token.methods
+    .transferAndCall(mediator.options.address, value, `${tokenReceiver.options.address}1122`)
     .send()
   const relayTxHash1 = await foreign.waitUntilProcessed(receipt1)
   const bridgedToken = await foreign.getBridgedToken(token)
 
   await foreign.checkTransfer(relayTxHash1, bridgedToken, foreign.mediator, tokenReceiver, home.reduceByHomeFee(value))
   assert.strictEqual(await tokenReceiver.methods.data().call(), '0x1122', 'Data was not passed correctly')
+
+  console.log('Sending 5 tokens to the Foreign Mediator with extra data')
+  const receipt2 = await bridgedToken.methods
+    .transferAndCall(foreign.mediator.options.address, toWei('5'), `${home.tokenReceiver.options.address}3344`)
+    .send()
+  const relayTxHash2 = await home.waitUntilProcessed(receipt2)
+
+  await home.checkTransfer(relayTxHash2, token, mediator, home.tokenReceiver, home.reduceByForeignFee(toWei('5')))
+  assert.strictEqual(await home.tokenReceiver.methods.data().call(), '0x3344', 'Data was not passed correctly')
 }
 
 module.exports = {
   name: 'Bridging of Home tokens with extra data',
-  shouldRun: ({ home }) => {
-    return home.mediator.methods.isTokenRegistered(home.token.options.address).call()
-  },
+  shouldRun: () => true,
   run,
 }
