e4b84cb6754f36ce7e619fac07b4407fe63ba5e6
Kirill Fedoseev <k.fedoseev@innopolis.university>
2021-08-16 10:53:09 +0300

    Bump EIP712 version in precompiles

--- precompiled/PermittableToken_flat.sol
+++ precompiled/PermittableToken_flat.sol
@@ -791,7 +791,7 @@ pragma solidity 0.4.24;
 
 
 contract PermittableToken is ERC677BridgeToken {
-    string public constant version = "1";
+    string public constant version = "2";
 
     // EIP712 niceties
     bytes32 public DOMAIN_SEPARATOR;
